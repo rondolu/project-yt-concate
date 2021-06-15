215a8ab8c55b950090041596aa2832ebc5e01864
Jacky Lu <jackylu0937648512@gmail.com>
2021-06-15 18:43:05 +0800

    Successfully download captions and save video list to file

--- yt_concate/main.py
+++ yt_concate/main.py
@@ -1,5 +1,9 @@
 from yt_concate.pipeline.steps.get_video_list import GetVideoList
 from yt_concate.pipeline.pipeline import Pipeline
+from yt_concate.pipeline.steps.preflight import Preflight
+from yt_concate.pipeline.steps.postflight import Postflight
+from yt_concate.pipeline.steps.download_captions import DownloadCaptions
+from yt_concate.utils import Utils
 
 CHANNEL_ID = 'UC-lHJZR3Gqxm24_Vd_AJ5Yw'
 
@@ -9,12 +13,14 @@ def main():
         'channel_id': CHANNEL_ID
     }
     steps = [
+        Preflight(),
         GetVideoList(),
-    ]
-
+        DownloadCaptions(),
+        Postflight(),
+        ]
+    utils = Utils()
     p = Pipeline(steps)
-    p.run(inputs)
-
+    p.run(inputs, utils)
 
 if __name__ == '__main__':
     main()
